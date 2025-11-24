7341e497effa3511b14f5abad1f965909174924f
Romaan Tariq <tm23834@gmail.com>
2025-11-23 23:11:12 -0500

    Implement populate() function with fetch logic (STEP 3–8)

--- js/json.js
+++ js/json.js
@@ -3,22 +3,26 @@ const header = document.querySelector('header');
 const section = document.querySelector('section');
 
 // STEP 3a: Create the asynchronous function populate()
-
+async function populate() {
     // Introducing JavaScript Object Notation (JSON): https://json.org/
     // STEP 4: Store the URL of a JSON file in a variable */
-    
+     const requestURL = 'js/iscream.json';
     // STEP 5: Use the new URL to create a new request object
-    
+    const request = new Request(requestURL);
     // STEP 6: Make a network request with the fetch() function, which returns a Response object
-    
+     const response = await fetch(request);
     // STEP 7: Capture the returned Response object and covert to a JSON object using json()
     
     // STEP 8: Output the iScream JSON object to the console 
-    
+      const jsonObj = await request.json();
+
+    console.log(jsonObj);
     // STEP 9a: Invoke the populateHeader function here, then build it below
     
     // STEP 10a: Invoke the showTopFlavors function here, then build it below
-    
+     populateHeader(jsonObj);
+    showTopFlavors(jsonObj);
+}
 
 
 // STEP 3b: Call the populate() function
