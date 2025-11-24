b8b69f272c4b275bae1de844960c17e5c1c8737f
Romaan Tariq <tm23834@gmail.com>
2025-11-23 23:05:23 -0500

    Add DOM references for header and section

--- js/json.js
+++ js/json.js
@@ -1,5 +1,6 @@
 /* STEP 2: Reference the HEADER and the SECTION elements with variables */
-
+const header = document.querySelector('header');
+const section = document.querySelector('section');
 
 // STEP 3a: Create the asynchronous function populate()
 
